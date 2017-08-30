a0bc105f9f9bff62f7203c55e24ec02b16ec195a
Colton Mathews <mathews.colton@gmail.com>
2017-08-30 15:25:21 -0600

    variables

---
+++ basicSyntaxAndStructure/variables.js
@@ -0,0 +1,17 @@
+// variable types
+// var fullName: string = "Colton Mathews";
+// let paidAccount: boolean = true;
+// const versionNumber: number = 1.3;
+// fullName = "Paige Mathews";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+    var greeting = "Hello there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Colton", "Mathews");
+//# sourceMappingURL=variables.js.map
